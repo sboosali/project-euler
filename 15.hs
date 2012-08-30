436e29c1d08e44ad80757245ace8151e27eb723d
Sam Boosalis <samboosalis@gmail.com>
2012-08-29 22:05:11 -0400

    dp under pad

--- 15.hs
+++ 15.hs
@@ -15,34 +15,39 @@ listToGrid xs = listToGrid' (sqrtI $ length xs) xs
           where (xs,zs) = splitAt n ys
 
 -- pascal's triangle
-routes :: Int -> Matrix Int
-routes n = (routes' 3 m)
-  where m = constMatrix 0 (n+3,n+3) // [((2,2),1)] :: Matrix Int
-        -- 3 <- first row/col is padding, second row/col init to 1
-        -- n+3 <- (+1) for dual grid, (+2) for padding on either side
+pascalsTriangle :: Int -> Matrix Int
+pascalsTriangle n = subMatrix (2,2) (d,d) $ pascalsTriangle' 3 m
+                    -- 3 <- first row/col is padding, second row/col init to 1
+                    -- subMatrix <- unpad
+  where d = 2*n+2
+        m = constMatrix 0 (d,d) // [((2,2),1)] :: Matrix Int
+        -- (+2) <- (+1) for dual grid, (+1) for padding first row/col
+        -- (*2) for bounds
         -- ((2,2),1)
         --  <- the only way to get to the upper left corner is to start there
-        routes' :: Int -> Matrix Int -> Matrix Int
-        routes' i m
-          | i > n+4 = m
-          | (i-2) > (if odd n then n+1 else n) `div` 2 = -- after antidiagonal
-            --eg  n==3  (4-2) leq (3+1) div 2  is still the antidiagonal
-            --eg  n==2  (3-2) leq 2 div 2  is still the antidiagonal
-            routes' (i+1) (m // [( (h,w), m!(h-1,w) + m!(h,w-1) )
-                                | (h,w) <- zip [i-2..n+2] $ reverse [i-2..n+2]])
-          | otherwise = -- through antidiagonal
-            routes' (i+1) $ (m // [( (h,w), m!(h-1,w) + m!(h,w-1) )
+        pascalsTriangle' :: Int -> Matrix Int -> Matrix Int
+        pascalsTriangle' i m
+          | i > d = m
+          | otherwise =
+            pascalsTriangle' (i+1) $ (m // [( (h,w), m!(h-1,w) + m!(h,w-1) )
                                   | (h,w) <- zip [2..i] $ reverse [2..i]])
             -- grow minor antidiagonals from upper left
             -- the number of ways to get to this cell is
             -- the number of ways to get to the above cell plus
             -- the number of ways to get to the cell on the left
 
+f n = (pascalsTriangle $ m)!(m,m) where m = n+1
+
 main = do
-  assert $ (routes 2)!(4,4) == 6
-
   let k = 2
-  printGrid $ listToGrid $ (elems $ routes k)
+  printGrid $ listToGrid $ (elems $ pascalsTriangle k)
 
-  let k = 4
-  printGrid $ listToGrid $ (elems $ routes k)
+  let k = 3
+  printGrid $ listToGrid $ (elems $ pascalsTriangle k)
+
+  assert $ f 2 == 6
+  assert $ f 3 == 20
+  assert $ f 4 == 70
+
+  print $ f 20
+  -- 137846528820  
